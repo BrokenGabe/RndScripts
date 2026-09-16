1dcde23153624b470d212f6a6d91de2977a07b3c
BrokenGabe <taek@taek.us>
2026-09-15 20:17:51 -0700

    Added pytrhon temp conversion

---
+++ python/conv_temp.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+import sys
+
+def f2c(fahrenheit):
+	return (fahrenheit - 32) * 5/9
+
+def c2f(celsius):
+	return (celsius * 9 / 5) + 32
+
+if len(sys.argv) != 3:
+	print("Usage: ")
+	print(f" python {sys.argv[0]} f2c temp")
+	print(f" python {sys.argv[0]} c2f temp")
+	sys.exit(1)
+
+conversion = sys.argv[1]
+temperature = float(sys.argv[2])
+
+if conversion == "f2c":
+	result = f2c(temperature)
+elif conversion == "c2f":
+	result = c2f(temperature)
+else:
+	print("Error: conversion must be 'f2c' or 'c2f'")
+
+print(f"{result:.1f}")
